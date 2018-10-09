e9a199c51e7570bec7add8e6fba1fdf0e7e9c513
Alex Kozlov <kau88@rambler.ru>
2018-10-09 18:23:12 +0300

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $('.slider').slick({
+    // autoplay: true,
+    arrows: false,
+    dots: true,
+    dotsClass: "my-dots",
+    slidesToShow: 1,
+		slidesToScroll: 1,
+		
+  });
+});
